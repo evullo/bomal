1117236fb3e9d6777c24bef8e7568a60fb4c4a49
Vullo Enzo <e.vullo@student.helmo.be>
2024-09-03 20:16:02 +0200

    Pending about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import ParagraphItem from '@/components/items/ParagraphItem.vue'
+import HeaderOneItem from '@/components/items/HeaderOneItem.vue'
 </script>
 
 <template>
   <main class="w-11/12 bg-orange-100 mx-auto rounded-md px-4 py-2 my-5 md:w-9/12">
-    <h1>About</h1>
+    <HeaderOneItem>Découvrez notre histoire</HeaderOneItem>
+    <ParagraphItem>Cette section racontera l'histoire des établissments Bomal</ParagraphItem>
   </main>
 </template>
